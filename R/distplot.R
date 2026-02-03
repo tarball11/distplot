870da9d7ec05ee6d0fb2c54b02fa88ef5f68e658
Travis Carter <travis.carter@gmail.com>
2026-02-03 14:02:09 -0500

    Renaming files.

---
+++ R/distplot.R
@@ -0,0 +1,19 @@
+#' distplot: A package for generating statistical distributions with simple,
+#' minimalist aesthetics.
+#'
+#' The distplot package has functions to generate several types of distributions
+#' based on provided parameters (e.g., normal, t, and F distributions), with
+#' optional shading and added line segments.
+#'
+#'
+#' @docType package
+#'
+#' @importFrom magrittr %>%
+#' @importFrom rlang .data
+#' @importFrom rlang %||%
+#' @importFrom ggplot2 %+replace%
+#'
+#' @name distplot
+NULL
+
+
